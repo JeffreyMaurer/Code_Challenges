f4ad3b3d18bf7ae52d726d3b167807f87c5f392c
JeffreyMaurer <maurerj@bu.edu>
2018-11-14 12:54:49 -0500

    added elu

--- PokemonAI/NeuralNetwork.py
+++ PokemonAI/NeuralNetwork.py
@@ -12,56 +12,60 @@ def sigmoid(x, deriv=False):
         return x * (1-x)
     return 1/(1+np.exp(-x))
 
-
 def relu(x, deriv=False):
     z = np.array(x)
     z[z<0] = 0
-    z = z/np.abs(np.max(z))
     if (deriv==True):
         z[z>0] = 1
     return z
 
 def leaky_relu(x, deriv=False):
     z = np.array(x)
-    z = z/np.abs(np.max(z))
     z[z<0] *= 0.01
     if (deriv==True):
         z[z>0] = 1
         z[z<=0] = 0.01
     return z
 
-
 def softplus(x, deriv=False):
     z = np.array(x)
-    z = z/np.abs(np.max(z))
     if (deriv==True):
         return z
     return np.log(1+np.exp(z))
+
+def elu(x, deriv=False):
+    z = np.array(x)
+    z[z<=0] = np.exp(z[z<=0]) - 1
+    if (deriv==True):
+        z[z>0] = 1
+        z[z<=0] = z[z<=0] + 1
+    return z
 
 # np.random.seed(1)
 
 class NeuralNetwork:
     """
-    This class describes a fully connected feed-forward neural network that is 
+    This class describes a fully connected feed-forward neural network that is
     capable of updating weights through backpropagation and genetic algorithms.
     """
 
     next_ID = 0
-    learning_rate = 0.01
+    learning_rate = 1/(1000*10)
 
-    def __init__(self, HyperParams, nonlin=sigmoid):
+    def __init__(self, HyperParams, nonlin=sigmoid, learning_rate=0):
         """
         description: This initializes the network's architecture, weights and name.
         params:
             HyperParams: iterable of integers, signifying how many nodes in each layer
             nonlin: the activation function to use for all layers
-        output: 
+        output:
             self: the neural network
         modified:
             self.synapses: a list of numpy matrices, the weights of the network
             self.score: number, the networks's "fitness" (used for genetic algorithm)
             self.name: integer, the identity of the network
             self.nonlin: the network's activation function
+            self.lr: The network's default learning rate
         """
         self.synapses = []
         for synapse in range(len(HyperParams)-1):
@@ -69,14 +73,15 @@ class NeuralNetwork:
         self.score = 0
         self.name = str(NeuralNetwork.next_ID)
         NeuralNetwork.next_ID += 1
-        self.nonlin=nonlin
-        
+        self.nonlin = nonlin
+        self.lr = learning_rate if learning_rate != 0 else NeuralNetwork.learning_rate
+
     def feed(self, state):
         """
         description: This is the main function of the neural network, it produces output from input
         params:
             state: a numpy vector or matrix of floats or integers, the input to the neural network
-        output: 
+        output:
             self.layers[-1]: a numpy vector or matrix of floats, the final output of the network
         modified:
             self.layers: a list of numpy vectors or matrices of numbers, the input and output of each layer
@@ -91,9 +96,9 @@ class NeuralNetwork:
         """
         description: This is the second main function of the neural network, for training of single examples at a time
         params:
-            error: a numpy vector of floats or integers, the difference between the 
+            error: a numpy vector of floats or integers, the difference between the
             actual output of feed and the desired output
-        output: 
+        output:
             None
         modified:
             self.synapses: a list of numpy vectors or matrices of numbers, the weights between each layer
@@ -101,15 +106,15 @@ class NeuralNetwork:
         for j in range(1,1+len(self.synapses)):
             delta = error * self.nonlin(self.layers[-j], True)
             error = delta.dot(self.synapses[-j].T)
-            self.synapses[-j] += NeuralNetwork.learning_rate*np.outer(self.layers[-(j+1)].T, delta)
+            self.synapses[-j] += np.outer(self.layers[-(j+1)].T, delta)
 
     def backprop_batch(self, error):
         """
         description: This is the second main function of the neural network, for training of batches
         params:
-            error: a numpy matrix of floats or integers, the difference between the 
+            error: a numpy matrix of floats or integers, the difference between the
             actual output of feed and the desired output
-        output: 
+        output:
             None
         modified:
             self.synapses: a list of numpy vectors or matrices of numbers, the weights between each layer
@@ -120,51 +125,54 @@ class NeuralNetwork:
             self.synapses[-j] += self.layers[-(j+1)].T.dot(delta)
 
 
-    def train_batch(self, state, outcome, epoch = 10*1000):
-        """
-        description: The function to train the neural network using batches
-        params:
-            state: a numpy matrix of floats or integers, the input to the neural network
-            outcome: a numpy matrix of floats or integers, the desired output
-            epoch: integer, the number of iterations of training on this batch
-        output: 
-            Every 100 epoch, the absolute means error is printed to the command line
-        modified:
-            self.synapses: a list of numpy matrices of numbers, the weights between each layer
-            self.layers: a list of numpy matrices of numbers, the input and output of each layer
-        """
-        for i in range(epoch):
-            output = self.feed(state)
-
-            error = outcome - output
-            if (i % (epoch//100)) == 0: print(str(np.mean(np.abs(error))))
-
-            self.backprop_batch(error)
-
-    def train_simple(self, state, outcome, epoch=1):
+    def train_simple(self, state, outcome, epoch=1, lr=0):
         """
         description: The function to train the neural network using individual examples
         params:
             state: a numpy vector of floats or integers, the input to the neural network
             outcome: a numpy vector of floats or integers, the desired output
             epoch: integer, the number of iterations of training on this example
-        output: 
+        output:
             None
         modified:
             self.synapses: a list of numpy vectors of numbers, the weights between each layer
             self.layers: a list of numpy vectors of numbers, the input and output of each layer
         """
+        lr = self.lr if lr == 0 else lr
         for i in range(epoch):
             output = self.feed(state)
             error = outcome - output
-            self.backprop(error)
-        
+            self.backprop(lr*error)
+
+
+    def train_batch(self, state, outcome, epoch = 10*1000, lr=0):
+        """
+        description: The function to train the neural network using batches
+        params:
+            state: a numpy matrix of floats or integers, the input to the neural network
+            outcome: a numpy matrix of floats or integers, the desired output
+            epoch: integer, the number of iterations of training on this batch
+        output:
+            Every 100 epoch, the absolute means error is printed to the command line
+        modified:
+            self.synapses: a list of numpy matrices of numbers, the weights between each layer
+            self.layers: a list of numpy matrices of numbers, the input and output of each layer
+        """
+        lr = self.lr if lr == 0 else lr
+        for i in range(epoch):
+            output = self.feed(state)
+
+            error = outcome - output
+            if (i % (epoch//100)) == 0: print(str(np.mean(np.abs(error))))
+
+            self.backprop_batch(lr*error)
+
     def next_gen(self):
         """
         description: The function to train the neural network using a genetic algorithm
         params:
             None
-        output: 
+        output:
             child: a neural network with weights that are similar to self
         modified:
             None
@@ -176,26 +184,30 @@ class NeuralNetwork:
         child.name += "<-" + self.name
         return(child)
 
-    def train_replay(self, replay, attack_freq): # s, a, r, s'
+    def train_replay(self, replay, attack_freq, lr=0): # s, a, r, s'
+        lr = self.lr if lr == 0 else lr
         state, action, reward, state_prime = replay
         Q = self.feed(state)
-        Q[action] = reward * attack_freq[action] / sum(attack_freq) # + y * max(np.multiply(Q.feed(state), mask))
-        self.backprop(Q)
+        error = Q[action] - reward * attack_freq[action] / sum(attack_freq) # + y * max(np.multiply(Q.feed(state), mask))
+        self.backprop(lr*error)
 
-    def train_replay_batch(self, replays, attack_freq): # s, a, r, s'
-        #states = np.array([replay[0] for replay in replays]) # 4 x 50
-        #vector_actions = np.array([replay[1] for replay in replays]) # 1 x 50
-        #matrix_actions = np.zeros((len(attack_freq),len(replays))
-        #matrix_actions[np.arange(len(replays)), vector_actions] = 1
-        #rewards = np.array([replay[2] for replay in replays]) # 1 x 50
-        #Q = self.feed(states)
-        #Q[np.arange(len(replays)), vector_actions] = rewards * attack_freq[matrix_actions] / sum(attack_freq) # + y * max(np.multiply(Q.feed(state), mask))
-        #self.backprop_batch(Q)
-        pass
+    def train_replay_batch(self, replays, batch_size, epoch=100, lr=0): # s, a, r, s'
+        lr = self.lr if lr == 0 else lr
+        for i in range(epoch):
+            replay_sample = random.sample(replays, batch_size)
+            #batch_size = len(replay_sample)
+            states = np.array([r[0] for r in replay_sample])
+            attack_indices = np.array([r[1] for r in replay_sample])
+            rewards = np.array([r[2] for r in replay_sample])
+            Q_values = self.feed(states)
+            errors = np.zeros(Q_values.shape)
+            errors[np.arange(batch_size),attack_indices] = rewards - Q_values[np.arange(batch_size),attack_indices]
+            print(np.sum(np.square(errors))/errors.shape[0])
+            self.backprop_batch(lr*errors)
 
-# As a show of how the neural network can be used, 
+# As a show of how the neural network can be used,
 # here is the neural network solving the xor problem
-    
+
 # input data
 X = np.array([[0,0,1],
             [0,1,1],
@@ -210,8 +222,6 @@ y = np.array([[0],
              [0]])
 
 if __name__ == "__main__":
-    NN = NeuralNetwork((3,10,10,1), relu)
-    NN.train_batch(X, y, 10000)
+    NN = NeuralNetwork((3,5,1), elu)
+    NN.train_batch(X, y, 1000)
     print(NN.feed(X))
-
-
